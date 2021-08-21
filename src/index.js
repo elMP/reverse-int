479ea095169e2dd81c285b0c9e423e5b32b7aff5
kneazle <kneazle@yandex.ru>
2021-08-21 17:12:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  const stringNumber = String(n);
+  let newString = '';
+  for (let i = stringNumber.length - 1; i >= 0; i--) {
+    if (stringNumber[i] === '-')
+      break;
+
+    newString += stringNumber[i];
+  }
+
+  return Number(newString);
 }
